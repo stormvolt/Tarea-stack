849a5ff59d58716424814fe35a44d7808d31dde6
luis <luisnu97@hotmail.com>
2015-11-15 14:35:31 -0500

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+ 
+#include "stack.h"
+#include "stack.cpp"
+ 
+using namespace std;
+
+//template<class T> class Stack;
+
+int main()
+{
+	Stack<int> iStack;
+	iStack.push(20);
+	iStack.push(10);
+	cout << iStack.pop() << ",";
+	iStack.push(40);
+	iStack.push(30);
+
+	cout << iStack.pop() << ",";
+	cout << iStack.pop() << ",";
+	iStack.push(90);
+	cout << iStack.pop() << ",";
+	cout << iStack.pop() << endl;
+
+	return 1;
+}
