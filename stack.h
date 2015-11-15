08cc8e9a024d4e0198207e21559b585a69a0ad23
luis <luisnu97@hotmail.com>
2015-11-15 14:33:43 -0500

    stack.h

---
+++ stack.h
@@ -0,0 +1,29 @@
+#ifndef STACK_H
+#define STACK_H
+ 
+
+#include <iostream>
+#include "node.h"
+#include "node.cpp"
+ 
+ 
+template <class T>
+ 
+class Stack
+{
+	private:
+		Node<T> *last;
+		
+	public:
+		// Constructor por defecto	        
+		Stack() : last(NULL){};
+	        ~Stack();
+ 
+		void push(T);
+		T pop();
+	
+		 
+
+};
+ 
+#endif // LIST_H
