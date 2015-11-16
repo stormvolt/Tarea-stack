3291ddf56d413388fbb81a8c66947dcb2e8d540d
stormvolt <luisnu97@hotmail.com>
2015-11-16 17:19:14 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,17 +10,26 @@ using namespace std;
 int main()
 {
 	Stack<int> iStack;
+	cout << "Añadimos a la pila el valor 20." ;
 	iStack.push(20);
+	cout << "Añadimos a la pila el valor 20.";
 	iStack.push(10);
-	cout << iStack.pop() << ",";
+	cout << "Funcion pop: ";
+	cout << iStack.pop() ;
+	cout << "Añadimos a la pila el valor 40.";
 	iStack.push(40);
 	iStack.push(30);
 
-	cout << iStack.pop() << ",";
-	cout << iStack.pop() << ",";
+	cout << "Funcion pop: ";
+	cout << iStack.pop() ;
+	cout << "Funcion pop: ";
+	cout << iStack.pop() ;
+	cout << "Añadimos a la pila el valor 90.";
 	iStack.push(90);
-	cout << iStack.pop() << ",";
-	cout << iStack.pop() << endl;
+	cout << "Funcion pop: ";
+	cout << iStack.pop() ;
+	cout << "Funcion pop: ";
+	cout << iStack.pop() ;
 
 	return 1;
 }
